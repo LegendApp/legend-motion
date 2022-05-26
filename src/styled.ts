c225143014f7de23af4e4d11f791c1a5147b43a3
Jay Meistrich <jmeistrich@gmail.com>
2022-05-27 00:21:08 +0800

    Improve typing of styled components

--- src/styled.ts
+++ src/styled.ts
@@ -1,13 +1,21 @@
-import { Animated } from 'react-native';
+import type { ComponentType } from 'react';
+import { Animated, FlatListProps, ImageProps, ScrollViewProps, SectionListProps, TextProps, ViewProps } from 'react-native';
 // @ts-ignore
 import { styled } from 'tailwindcss-react-native';
 import { createMotionComponent } from './createMotionComponent';
 
+interface StyledProps {
+    className?: string;
+    tw?: string;
+}
+
 export namespace Motion {
-    export const View = createMotionComponent(styled(Animated.View));
-    export const Text = createMotionComponent(styled(Animated.Text));
-    export const FlatList = createMotionComponent(styled(Animated.FlatList));
-    export const Image = createMotionComponent(styled(Animated.Image));
-    export const ScrollView = createMotionComponent(styled(Animated.ScrollView));
-    export const SectionList = createMotionComponent(styled(Animated.SectionList));
+    export const View = createMotionComponent<ComponentType<ViewProps & StyledProps>>(styled(Animated.View));
+    export const Text = createMotionComponent<ComponentType<TextProps & StyledProps>>(styled(Animated.Text));
+    export const FlatList = createMotionComponent<ComponentType<FlatListProps<unknown> & StyledProps>>(styled(Animated.FlatList));
+    export const Image = createMotionComponent<ComponentType<ImageProps & StyledProps>>(styled(Animated.Image));
+    export const ScrollView = createMotionComponent<ComponentType<ScrollViewProps & StyledProps>>(styled(Animated.ScrollView));
+    export const SectionList = createMotionComponent<ComponentType<SectionListProps<unknown, unknown> & StyledProps>>(
+        styled(Animated.SectionList)
+    );
 }
